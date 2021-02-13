88031f1c573c23ac32018b9e9929299dfae15dcf
Nick <b10303008@gmail.com>
2021-02-13 22:52:12 +0800

    v1 api/report

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 import { connectDB } from "./database";
-import { getRepository } from "typeorm";
+import { getRepository, MoreThan } from "typeorm";
 import { Task } from "./entity/Task";
+import * as moment from 'moment';
 
 const startServer = () => {
     // create and setup express app
@@ -26,7 +27,20 @@ const startServer = () => {
     app.patch("/api/tasks/:id", async function (req: Request, res: Response) {
         const taskRepository = getRepository(Task);
         const task = await taskRepository.findOne(req.params.id);
-        taskRepository.merge(task, req.body);
+        const {
+            content,
+            completed
+        } = req.body;
+
+        let updateData = {};
+        if (content) {
+            updateData['updateData'] = content;
+        }
+        if (completed) {
+            updateData['completed'] = completed;
+            updateData['completedAt'] = completed ? moment() : null;
+        }
+        taskRepository.merge(task, updateData);
         const result = await taskRepository.save(task);
         res.status(200).json({ status: 200, data: result });
     });
@@ -35,6 +49,83 @@ const startServer = () => {
         const taskRepository = getRepository(Task);
         await taskRepository.delete(req.params.id);
         res.status(200).json({ status: 200, data: {} });
+    });
+
+    app.get("/api/reports", async function (req: Request, res: Response) {
+        interface Report {
+            date: string,
+            createdTotal: number,
+            completedTotal: number
+        }
+        interface ReportMap {
+            [name: string]: Report
+        }
+        interface ResponseData {
+            today: {
+                createdTotal: number,
+                completedTotal: number,
+            },
+            weeklyReport: {
+                createdTotal: number,
+                completedTotal: number,
+            },
+            dailyReport: Array<Report>
+        }
+        const DateFormatType: string = 'MM/DD';
+        const taskRepository = getRepository(Task);
+        const reportStartDate = moment().subtract(6, 'day').format('YYYY-MM-DD');
+        const createdTasksPromise = taskRepository.find({
+            createdAt: MoreThan(reportStartDate)
+        });
+        const completedTasksPromise = taskRepository.find({
+            completedAt: MoreThan(reportStartDate)
+        });
+        const [createdTasks, completedTasks] = await Promise.all([createdTasksPromise, completedTasksPromise]);
+
+        const reportMap: ReportMap = Array(7)
+            .fill('')
+            .map((v, idx) => idx)
+            .reduce((counter, dateDiff) => {
+                const momentObj = moment().subtract(dateDiff, 'day');
+                const report: Report = { date: momentObj.format(DateFormatType), createdTotal: 0, completedTotal: 0 };
+                counter[momentObj.format(DateFormatType)] = report;
+                return counter;
+            }, {});
+
+        createdTasks.forEach(task => {
+            const date = moment(task.createdAt).format(DateFormatType);
+            const newReport = { ...reportMap[date] };
+            newReport.createdTotal += 1;
+            reportMap[date] = newReport;
+        })
+
+        completedTasks.forEach(task => {
+            const date = moment(task.completedAt).format(DateFormatType);
+            const newReport = { ...reportMap[date] };
+            newReport.completedTotal += 1;
+            reportMap[date] = newReport
+        })
+
+        const todayString = moment().format(DateFormatType);
+        const reposts = Object.values(reportMap);
+        const weeklyReport = reposts.reduce((result, rep: Report) => {
+            result.createdTotal += rep.createdTotal;
+            result.completedTotal += rep.completedTotal;
+            return result;
+        }, { createdTotal: 0, completedTotal: 0 })
+        const sortByDateAsc = (r1: Report, r2: Report): number => (r1.date < r2.date ? -1 : 1);
+        const responseData: ResponseData = {
+            today: {
+                createdTotal: reportMap[todayString].createdTotal,
+                completedTotal: reportMap[todayString].completedTotal
+            },
+            weeklyReport: {
+                createdTotal: weeklyReport.createdTotal,
+                completedTotal: weeklyReport.completedTotal
+            },
+            dailyReport: reposts.sort(sortByDateAsc)
+        }
+        res.status(200).json({ status: 200, data: responseData });
     });
 
     // start express server
