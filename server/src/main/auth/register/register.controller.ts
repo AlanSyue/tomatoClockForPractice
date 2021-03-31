01c7de85cc1d9446072bf7bcc3fe4e57b041834f
cyd <michellechen58@gmail.com>
2021-03-31 15:12:26 +0800

    express-validator

--- server/src/main/auth/register/register.controller.ts
+++ server/src/main/auth/register/register.controller.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { getRepository } from 'typeorm';
 import { User } from '../../../entity/User';
-import * as EmailValidator from 'email-validator';
 import { body, validationResult, check } from 'express-validator';
+import { transporter } from '../mail/nodemailer.controller'
 
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
@@ -11,55 +11,29 @@ function generateAccessToken(username) {
   return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '2000000000000000000s' });
 }
 
-function hasCapital(str)
-{
-    var result = str.match(/^.*[A-Z]+.*$/);
-    if(result==null) return false;
-    return true;
-}
-
-function hasLowercase(str)
-{
-    var result = str.match(/^.*[a-z]+.*$/);
-    if(result==null) return false;
-    return true;
-}
-
-function hasNumber(str)
-{
-    var result = str.match(/^.*[0-9]+.*$/);
-    if(result==null) return false;
-    return true;
-}
-
-
 export const register = async function (req: Request, res: Response, next: NextFunction) {
-    const userRepo = await getRepository(User);
-    const password = req.body.password;
-    const userEmail = req.body.email;
-
     const checkEmail = await check('email').notEmpty().isEmail().run(req, { dryRun: true });
     if(!checkEmail.isEmpty()){
         res.status(401).json({status:401, errors: "email 格式錯誤" });
     }
     
-    const checkPassword = await check('password').notEmpty().isLength({ min: 8 }).run(req, { dryRun: true });
-    if(!checkPassword.isEmpty() || hasCapital(password)==false || hasLowercase(password)==false || hasNumber(password) == false){
+    const checkPassword = await check('password').notEmpty().isLength({ min: 8 }).matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, "i").run(req, { dryRun: true });
+    if(!checkPassword.isEmpty()){
         res.status(401).json({status:401, errors: "密碼需包含英文大小寫和數字，長度超過8位數" });
     }
 
-    const emailRepo = await userRepo.find({where: { email: userEmail }});
+    const emailRepo = await getRepository(User).find({where: { email: req.body.email }});
     if(emailRepo.length != 0){
         res.status(401).json({status:401, errors: "email has been used" });
     }
 
     else{
         const salt = await bcrypt.genSalt(10);
-        req.body.password = await bcrypt.hash(password, salt);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
         const token = generateAccessToken({ username: req.body.email });
         req.body.verifiedCode = token;
-        const userCreate = await userRepo.create(req.body);
-        const results = await userRepo.save(userCreate);
+        const userCreate = await getRepository(User).create(req.body);
+        const results = await getRepository(User).save(userCreate);
         res.status(200).json({status:200, messgae: "please verify email"});
     }
 }
